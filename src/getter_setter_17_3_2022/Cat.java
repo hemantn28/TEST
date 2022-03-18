eb502a836f5e7fcaa07a13f43c80e13924ea2e57
Hemant <hemantnagar28@yahoo.com>
2022-03-18 06:11:34 +0530

    committ

---
+++ src/getter_setter_17_3_2022/Cat.java
@@ -0,0 +1,33 @@
+package getter_setter_17_3_2022;
+
+
+public class Cat {
+
+    private String name="AShley";
+    private float weight=5.5f;
+    private int age=3;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
